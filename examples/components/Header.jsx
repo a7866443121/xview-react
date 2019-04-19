432eab8dd5b10a71e2ff7e0b2ab5c5af447ad6af
liukeli <kl6695732@qq.com>
2019-04-19 16:28:38 +0800

    初步完成搭建

--- examples/components/Header.jsx
+++ examples/components/Header.jsx
@@ -14,7 +14,7 @@ export default class Header extends Component {
           style={{boxShadow: pageName === 'index' ? 'none' : '0 10px 60px 0 rgba(29,29,31,0.07)'}}
         >
           <div className="header-container">
-            <a href="./index'"><img src="../../static/img/logo.png" width="100" /></a>
+            <a href="./index'"><img src="./static/img/logo.png" width="100" /></a>
             <a href="./giud">组件</a>
           </div>
         </div>
